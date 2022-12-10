473459d42440ce6f641b2a6f2f235df3a2a3b789
Melisa Tanrıkulu <melisa.tanrikulu@ug.bilkent.edu.tr>
2022-12-10 21:03:05 +0300

    Filter is added (courses->add course section)

--- erasmus/src/erasmus/accounts/static/manage.js
+++ erasmus/src/erasmus/accounts/static/manage.js
@@ -1,23 +1,25 @@
-<script>
 function myFunction() {
+
+console.log("works");
   // Declare variables
-  var input, filter, table, tr, td, i, txtValue;
+  var input, filter, table, row, courseName, courseID, i, courseTxtValue, idTxtValue;
   input = document.getElementById("myInput");
   filter = input.value.toUpperCase();
-  table = document.getElementById("taken_courses_table");
-  tr = table.getElementsByTagName("tr");
+  table = document.getElementById("course_list_table");
+  row = table.getElementsByTagName("tr");
 
   // Loop through all table rows, and hide those who don't match the search query
-  for (i = 0; i < tr.length; i++) {
-    td = tr[i].getElementsByTagName("td")[0];
-    if (td) {
-      txtValue = td.textContent || td.innerText;
-      if (txtValue.toUpperCase().indexOf(filter) > -1) {
-        tr[i].style.display = "";
+  for (i = 0; i < row.length; i++) {
+    courseName = row[i].getElementsByTagName("td")[1];
+    courseID = row[i].getElementsByTagName("td")[2];
+    if (courseName) {
+      courseTxtValue = courseName.textContent || courseName.innerText;
+      idTxtValue = courseID.textContent || courseID.innerText;
+      if (courseTxtValue.toUpperCase().indexOf(filter) > -1 || idTxtValue.toUpperCase().indexOf(filter) > -1) {
+        row[i].style.display = "";
       } else {
-        tr[i].style.display = "none";
+        row[i].style.display = "none";
       }
     }
   }
 }
-</script>;
