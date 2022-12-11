593c6358dc9b8f7694f1f6e3ade74e5fbd7792fa
Ayşe Kelleci <70059998+aysekelleci@users.noreply.github.com>
2022-12-11 22:51:02 +0300

    small error

--- erasmus/src/erasmus/accounts/context_processors.py
+++ erasmus/src/erasmus/accounts/context_processors.py
@@ -7,18 +7,20 @@ def get_user_type(request):
     if request is None:
         return user_type
 
+
     Students = Student.objects.all()
     Coordinators = Student.objects.all()
-
     user = request.user
-    erasmus_user = ErasmusUser.objects.filter(user=user).first()
-    if erasmus_user is not None:
-        if Coordinator.objects.filter(user=erasmus_user).first():
-            user_type = "Coordinator"
-        elif Student.objects.filter(user=erasmus_user).first():
-            user_type = "Student"
-        else:
-            user_type = "Board Member"
+    if user.is_authenticated:
+        erasmus_user = ErasmusUser.objects.filter(user=user).first()
+        if erasmus_user is not None:
+            if Coordinator.objects.filter(user=erasmus_user).first():
+                user_type = "Coordinator"
+            elif Student.objects.filter(user=erasmus_user).first():
+                user_type = "Student"
+            else:
+                user_type = "Board Member"
+
     return {'user_ty': user_type}
 
 
