acad6c4195392154eaa9441628a370027c659db3
Ayşe Kelleci <70059998+aysekelleci@users.noreply.github.com>
2022-12-11 19:40:15 +0300

    global method
    you can access user type in every html page by using {{user_type}}

---
+++ erasmus/src/erasmus/accounts/context_processors.py
@@ -0,0 +1,24 @@
+from .models import User
+from .models import ErasmusUser, Student, Coordinator, BoardMember
+
+
+def get_user_type(request):
+    user_type = None
+    if request is None:
+        return user_type
+
+    Students = Student.objects.all()
+    Coordinators = Student.objects.all()
+
+    user = request.user
+    erasmus_user = ErasmusUser.objects.filter(user=user).first()
+    if erasmus_user is not None:
+        if Coordinator.objects.filter(user=erasmus_user).first():
+            user_type = "Coordinator"
+        elif Student.objects.filter(user=erasmus_user).first():
+            user_type = "Student"
+        else:
+            user_type = "Board Member"
+    return {'user_ty': user_type}
+
+
