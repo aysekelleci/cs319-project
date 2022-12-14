17398a77d953cf154c63f887f0f789ecb2c70e42
Melisa Tanrıkulu <melisa.tanrikulu@ug.bilkent.edu.tr>
2022-12-14 21:24:09 +0300

    Navbar and FAQ update
    Navbar buttons are colored when the corresponding page is on display, FAQ has a adjust button for nonstudent users

--- erasmus/src/erasmus/accounts/static/manage.js
+++ erasmus/src/erasmus/accounts/static/manage.js
@@ -1,3 +1,20 @@
+window.onload = function init() {
+    let buttonID = "1-holder";
+    let textID = "1";
+
+    if (localStorage.clickedButton != "0") {
+        buttonID = localStorage.clickedButton + "-holder";
+        textID = localStorage.clickedButton;
+    }
+
+    button = document.getElementById(buttonID);
+    text = document.getElementById(textID);
+    button.style.backgroundColor = "#db1430";
+    text.style.color = "#fff";
+
+    localStorage.clickedButton = "0";
+}
+
 function filterTable() {
   // Declare variables
   var input, filter, table, row, courseName, courseID, i, courseTxtValue, idTxtValue;
@@ -38,4 +55,42 @@ function toggleButtons(button, count) {
             element.setAttribute("hidden", "hidden");
     }
     console.log(button_id);
+}
+
+function clicked(button)
+{
+    localStorage.clickedButton = button.id;
+
+    if (storageAvailable('localStorage')) {
+      console.log("storage works");
+    }
+    else {
+      console.log("storage dows not work");
+    }
+    console.log("clicked");
+}
+
+function storageAvailable(type) {
+    let storage;
+    try {
+        storage = window[type];
+        const x = '__storage_test__';
+        storage.setItem(x, x);
+        storage.removeItem(x);
+        return true;
+    }
+    catch (e) {
+        return e instanceof DOMException && (
+            // everything except Firefox
+            e.code === 22 ||
+            // Firefox
+            e.code === 1014 ||
+            // test name field too, because code might not be present
+            // everything except Firefox
+            e.name === 'QuotaExceededError' ||
+            // Firefox
+            e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
+            // acknowledge QuotaExceededError only if there's something already stored
+            (storage && storage.length !== 0);
+    }
 }
