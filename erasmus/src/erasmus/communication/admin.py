510630703550eee9fe0f0019e12fe2a91b12c6d7
KardelenCeren <kceren199@gmail.com>
2022-12-15 18:23:58 +0300

    Notification View

--- erasmus/src/erasmus/communication/admin.py
+++ erasmus/src/erasmus/communication/admin.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import FAQ, Question
+from .models import FAQ, Question, Notification
 
 admin.site.register(FAQ)
 
 admin.site.register(Question)
+
+admin.site.register(Notification)
--- erasmus/src/erasmus/communication/urls.py
+++ erasmus/src/erasmus/communication/urls.py
@@ -5,5 +5,6 @@ app_name = 'accounts'
 urlpatterns = [
     path('faq/', views.FAQView.as_view(),  name="faq"),
     path('add-question/', views.AddQuestion.as_view(), name = "add-question"),
+    path('notification/', views.NotificationView.as_view(), name = "notification"),
     path('', include('accounts.urls')),
 ]
