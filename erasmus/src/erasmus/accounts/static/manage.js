469cb798a76da39876651a79b7091f4e85d07a4a
Melisa Tanrıkulu <melisa.tanrikulu@ug.bilkent.edu.tr>
2022-12-17 23:12:00 +0300

    Forum table

--- erasmus/src/erasmus/accounts/static/manage.js
+++ erasmus/src/erasmus/accounts/static/manage.js
@@ -120,3 +120,28 @@ function popUp(message)
 {
     alert(message);
 }
+
+
+function filterForumTable() {
+  // Declare variables
+  var input, filter, table, row, courseName, courseID, i, courseTxtValue, idTxtValue;
+  input = document.getElementById("filter_input");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("filtered_list");
+  row = table.getElementsByTagName("tr");
+
+  // Loop through all table rows, and hide those who don't match the search query
+  for (i = 0; i < row.length; i++) {
+    courseName = row[i].getElementsByTagName("td")[0];
+    courseID = row[i].getElementsByTagName("td")[1];
+    if (courseName) {
+      courseTxtValue = courseName.textContent || courseName.innerText;
+      idTxtValue = courseID.textContent || courseID.innerText;
+      if (courseTxtValue.toUpperCase().indexOf(filter) > -1 || idTxtValue.toUpperCase().indexOf(filter) > -1) {
+        row[i].style.display = "";
+      } else {
+        row[i].style.display = "none";
+      }
+    }
+  }
+}
