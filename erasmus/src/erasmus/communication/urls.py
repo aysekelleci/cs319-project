64dbc69b8f2cc5b193fc187fe933eb4a57c84b73
Ayşe Kelleci <70059998+aysekelleci@users.noreply.github.com>
2022-12-17 13:36:17 +0300

    faq edit-delete controller

--- erasmus/src/erasmus/communication/urls.py
+++ erasmus/src/erasmus/communication/urls.py
@@ -5,6 +5,8 @@ app_name = 'accounts'
 urlpatterns = [
     path('faq/', views.FAQView.as_view(),  name="faq"),
     path('add-question/', views.AddQuestion.as_view(), name="add-question"),
+    path('delete-question/<int:question_id>', views.DeleteQuestionView.as_view(), name="delete-question"),
+    path('edit-question/<int:question_id>', views.EditQuestionView.as_view(), name="edit-question"),
     path('notification/', views.NotificationView.as_view(), name="notification"),
     path('', include('accounts.urls')),
 ]
