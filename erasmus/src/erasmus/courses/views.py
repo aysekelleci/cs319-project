0a35018cdd990a33b89791bbee90bbde1a76d838
KardelenCeren <kceren199@gmail.com>
2022-12-19 03:20:43 +0300

    Final course list submit fix

--- erasmus/src/erasmus/courses/views.py
+++ erasmus/src/erasmus/courses/views.py
@@ -293,7 +293,8 @@ class SubmitCourseListView(LoginRequiredMixin, View):
 
         # check if the user courses are all approved (merged or unmerged)
         for user_course in user_courses:
-            if (user_course.course.is_merged and not user_course.course.merged_course.approved) or (not user_course.course.approved):
+            if (user_course.course.is_merged and not user_course.course.merged_course.approved) \
+                    or (not user_course.course.is_merged and not user_course.course.approved):
                 messages.error(request, "You can only submit the final list only if all the courses are approved.")
                 return redirect('/courses')
 
